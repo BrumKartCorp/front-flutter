dcd07461ac06a83d5932f56b075282422f47caec
Quentin R <rouvilleq@gmail.com>
2022-03-10 12:41:54 +0100

    Make app working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:model_viewer/model_viewer.dart';
 void main(){
   runApp(new MaterialApp(
     home: new MyApp(),
