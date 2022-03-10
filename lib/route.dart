54a9510727f9cd56a6916dbc53297769283d5c8a
Rapha <rapha.israel@live.fr>
2022-03-10 18:51:33 +0100

    service connexion

--- lib/route.dart
+++ lib/route.dart
@@ -1,7 +1,24 @@
+import 'dart:convert';
+
+import 'package:brum_kart/class/destinations.dart';
 import 'package:flutter/material.dart';
 
 import 'MapSample.dart';
-import 'main.dart';
+import 'package:http/http.dart' as http;
+
+final String jsonplaceholder = "http://5.196.91.59:8001/path/get";
+
+Future<ApiResponse> fetchInfo() async {
+  final response = await http.get(
+    Uri.parse(jsonplaceholder),
+    headers: <String, String>{
+      'Content-Type': 'application/json'
+    },
+  );
+  final jsonResponse = json.decode(response.body);
+
+  return ApiResponse.fromJson(jsonResponse);
+}
 
 class ListRoute extends StatelessWidget {
   @override
@@ -20,13 +37,31 @@ class ListRoute extends StatelessWidget {
           ),
           automaticallyImplyLeading: false
       ),
-      body: new Center(
-        child: new Text("Il y aura ici une liste de destination!",
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+              child: Text(
+                'Listes des Destinations',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25.0),
+              ),
+            ),
           ),
-        ),
+          FutureBuilder(
+            future: fetchInfo(),
+            // ignore: missing_return
+
+            // ignore: missing_return
+            builder: (context, snapshot){
+              if(snapshot.hasData){
+                return new Text(snapshot.data[0].checkpoints[0].name);
+              } else if(snapshot.hasError) {
+                return new Text("Error");
+              }
+            },
+          )
+        ],
       ),
     );
   }
