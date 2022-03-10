101043c0aa44357e37a4a60c2639f22ef3d5f2f5
Cedric <cedric.lphn@yahoo.com>
2022-03-10 18:04:02 +0100

    fix: main dart

--- lib/view/authentification_view.dart
+++ lib/view/authentification_view.dart
@@ -21,10 +21,10 @@ class _AuthentificationView extends State<AuthentificationView> {
 
   getConnection(String user, String password) async {
     User data = await Brumkart.fetchUser(user);
-    if(data.password == password) {
+    if (data.password == password) {
       USER = data;
       print("hello");
-    }else {
+    } else {
       print("pas hello");
     }
   }
@@ -32,7 +32,7 @@ class _AuthentificationView extends State<AuthentificationView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
@@ -40,10 +40,7 @@ class _AuthentificationView extends State<AuthentificationView> {
           child: Column(
             children: <Widget>[
               Image(image: AssetImage('assets/images/logo.png')),
-              Card(
-                elevation: 2,
-                child: login()
-              )
+              Card(elevation: 2, child: login())
             ],
           ),
         ),
@@ -51,103 +48,92 @@ class _AuthentificationView extends State<AuthentificationView> {
     );
   }
 
-
   Widget login() {
     return Container(
         margin: EdgeInsets.fromLTRB(100, 10, 100, 10),
-      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              autofocus: false,
-              style: TextStyle(fontSize: 22.0, color: Color(0xFFbdc6cf)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                hintText: 'Morpheus',
-                contentPadding:
-                const EdgeInsets.only(left: 14.0, bottom: 12.0, top: 12.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                  borderRadius: BorderRadius.circular(25.7),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                  borderRadius: BorderRadius.circular(25.7),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              autofocus: false,
-              style: TextStyle(fontSize: 22.0, color: Color(0xFFbdc6cf)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                hintText: 'HDP',
-                contentPadding:
-                const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                  borderRadius: BorderRadius.circular(25.7),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                  borderRadius: BorderRadius.circular(25.7),
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: false,
+                style: TextStyle(fontSize: 22.0, color: Color(0xFFbdc6cf)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Morpheus',
+                  contentPadding: const EdgeInsets.only(
+                      left: 14.0, bottom: 12.0, top: 12.0),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                    borderRadius: BorderRadius.circular(25.7),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                    borderRadius: BorderRadius.circular(25.7),
+                  ),
                 ),
               ),
             ),
-          ),
-        Padding(
-          padding: EdgeInsets.all(10),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: Stack(
-              children: <Widget>[
-                Positioned.fill(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: <Color>[
-                          Color(0xFF911313),
-                          Color(0xFFB2362B)
-                        ]
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                autofocus: false,
+                style: TextStyle(fontSize: 22.0, color: Color(0xFFbdc6cf)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'HDP',
+                  contentPadding:
+                      const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                    borderRadius: BorderRadius.circular(25.7),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                    borderRadius: BorderRadius.circular(25.7),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(colors: <Color>[
+                              Color(0xFF911313),
+                              Color(0xFFB2362B)
+                            ]),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.white,
-                    textStyle: const TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (context) =>
-                            SimpleMap()
-                            print(user.value);
-                            getConnection(user.value.text, password.value.text);
-                    ));
-                  },
-                  child: const Text('Connexion'),
-                )
-              ],
-            )
-          ),
-        ),
-        TextButton(
-          onPressed: () => {
-
-          },
-          child: const Text('Inscription'),
-        ),
-        ],
-      )
-    );
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(16.0),
+                          primary: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          print(user.value);
+                          getConnection(user.value.text, password.value.text);
+                        },
+                        child: const Text('Connexion'),
+                      )
+                    ],
+                  )),
+            ),
+            TextButton(
+              onPressed: () => {},
+              child: const Text('Inscription'),
+            ),
+          ],
+        ));
   }
 }
