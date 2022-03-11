077b75cf53f1dcd2b0c7da75290040bf191e8934
Quentin R <rouvilleq@gmail.com>
2022-03-11 10:59:42 +0100

    Try to make buttons working

--- lib/3dScooter.dart
+++ lib/3dScooter.dart
@@ -11,6 +11,21 @@ class _ScooterView extends State<ScooterView> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
+    Color greenBackgroundColor = Colors.black;
+    Color redBackgroundColor = Colors.black;
+    Color flowerBackgroundColor = Colors.black;
+
+    String selectedColor = "";
+
+    ModelViewer md = ModelViewer(
+      src: 'assets/models/scuuter.glb',
+      alt: "A 3D model of a scuuter",
+      autoRotate: true,
+      cameraControls: true,
+      backgroundColor: Colors.blueAccent,
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -23,13 +38,7 @@ class _ScooterView extends State<ScooterView> {
           children: <Widget>[
             Container(
               height: height * 0.70,
-              child: ModelViewer(
-                src: 'assets/models/scuuter.glb',
-                alt: "A 3D model of a scuuter",
-                autoRotate: true,
-                cameraControls: true,
-                backgroundColor: Colors.blueAccent,
-              ),
+              child: md,
             ),
             Container(
               child: SingleChildScrollView(
@@ -38,96 +47,124 @@ class _ScooterView extends State<ScooterView> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
                     Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.orange,
-                              ),
-                              borderRadius: BorderRadius.all(Radius.circular(50)),
-                              color: Colors.black
-                          ),
-                          child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Image(
-                                  image:
-                                  AssetImage("assets/images/vert.jpg"),
-                                  height: 50,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            greenBackgroundColor = Colors.amber;
+
+                            redBackgroundColor = Colors.black;
+                            flowerBackgroundColor = Colors.black;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.orange,
                                 ),
-                                Center(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        text: "Vert",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    )
-                                )
-                              ]),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.orange,
-                              ),
-                              borderRadius: BorderRadius.all(Radius.circular(50)),
-                              color: Colors.black
-                          ),
-                          child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Image(
-                                  image:
-                                  AssetImage("assets/images/rouge.jpg"),
-                                  height: 50,
-                                ),
-                                Center(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        text: "Rouge",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    )
-                                )
-                              ]),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.orange,
-                              ),
-                              borderRadius: BorderRadius.all(Radius.circular(50)),
-                             color: Colors.black
-                          ),
-                          child: Column(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                color: greenBackgroundColor
+                            ),
+                            child: Column(
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
                                   Image(
-                                    image:
-                                    AssetImage("assets/images/motif-a-fleurs.jpg"),
+                                    image: AssetImage("assets/images/vert.jpg"),
                                     height: 50,
                                   ),
                                   Center(
                                       child: RichText(
-                                        text: TextSpan(
-                                          text: "Fleurs",
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                      )
-                                  )
+                                    text: TextSpan(
+                                      text: "Vert",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ))
                                 ]),
                           ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            redBackgroundColor = Colors.amber;
+
+                            greenBackgroundColor = Colors.black;
+                            flowerBackgroundColor = Colors.black;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.orange,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                color: redBackgroundColor
+                            ),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Image(
+                                    image: AssetImage("assets/images/rouge.jpg"),
+                                    height: 50,
+                                  ),
+                                  Center(
+                                      child: RichText(
+                                    text: TextSpan(
+                                      text: "Rouge",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ))
+                                ]),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            flowerBackgroundColor = Colors.amber;
+
+                            greenBackgroundColor = Colors.black;
+                            redBackgroundColor = Colors.black;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.orange,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                color: flowerBackgroundColor
+                            ),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Image(
+                                    image: AssetImage(
+                                        "assets/images/motif-a-fleurs.jpg"),
+                                    height: 50,
+                                  ),
+                                  Center(
+                                      child: RichText(
+                                    text: TextSpan(
+                                      text: "fleurs",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ))
+                                ]),
+                          ),
+                        ),
                       ),
                     )
                   ],
