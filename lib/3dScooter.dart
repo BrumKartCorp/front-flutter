c6dfbb117b347e47888b59177e735b8b80054e9a
Quentin R <rouvilleq@gmail.com>
2022-03-11 01:17:45 +0100

    Add scouter color changer

--- lib/3dScooter.dart
+++ lib/3dScooter.dart
@@ -9,16 +9,73 @@ class ScooterView extends StatefulWidget {
 class _ScooterView extends State<ScooterView> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Model Viewer")),
-        body: ModelViewer(
-          src: 'assets/models/scuuter.glb',
-          alt: "A 3D model of a scuuter",
-          ar: true,
-          autoRotate: true,
-          cameraControls: true,
-        ),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: height * 0.70,
+            child: ModelViewer(
+              src: 'assets/models/scuuter.glb',
+              alt: "A 3D model of a scuuter",
+              autoRotate: true,
+              cameraControls: true,
+            ),
+          ),
+          Container(
+            //height: height * 0.30,
+            child: SingleChildScrollView(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          title: Image(
+                            image: AssetImage("assets/images/bleu.jpg"),
+                            height: 50,
+                          ),
+                          subtitle: Text("Couleur bleu"),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          title: Image(
+                            image: AssetImage("assets/images/rouge.jpg"),
+                            height: 50,
+                          ),
+                          subtitle: Text("Couleur rouge"),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const ListTile(
+                            title: Image(
+                              image: AssetImage("assets/images/motif-a-fleurs.jpg"),
+                              height: 50,
+                            ),
+                            subtitle: Text("Motif à fleur"),
+                          )
+                        ]),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
